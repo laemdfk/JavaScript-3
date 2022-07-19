d2653082af46c5594c10eda5ce7f9bc4369b05e1
laemdfk <kappa021618@icloud.com>
2022-07-20 01:21:00 +0900

    Add files via upload

---
+++ JavaScript-3/javascript3.js
@@ -0,0 +1,12 @@
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 関数雨の呼出
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
+  let addStr = "Hello " + strA;
+  return addStr;
+}
